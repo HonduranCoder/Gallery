495a25abcf1a54828957cb68a0998b2b576c068d
Estefani <stef45689@gmail.com>
2021-09-22 18:59:05 -0500

    Refactored

--- src/App.js
+++ src/App.js
@@ -2,56 +2,51 @@ import React from 'react';
 import Header from './components/Header.js';
 import images from './components/data.js';
 import ImageList from './components/ImageList.js';
+import Dropdown from './components/Dropdown.js';
 import './App.css';
 
 export default class App extends React.Component {
   //State has a keyword that can start as an empty string-> no images, don't use the array use keyword or horns from data.js 
   state = {
-    keyword: ''
+    keyword: '',
+    horns: '',
     }
 
-    handleChange = (e) => {
+    handleKeyword = (e) => {
       this.setState({ keyword: e.target.value})
+    }
+    
+    handleHorns = (e) => {
+      this.setState({horns: Number(e.target.value)})
     }
   
   //function to filter the images array with the state option. Line 9.5 filter array = function(images). return all images that match the keyword in State
   render() {
-    const filteredImages = images.filter((image) => {
+    const filteredImages = images
+    .filter((image) => {
       if (this.state.keyword === '') return true
       if (image.keyword === this.state.keyword) return true
       return false
      }) 
-    return (
-      <>
+     .filter((image) => {
+      if (this.state.horns === '') return true
+      if (image.horns === this.state.horns) return true
+      return false
+     })
+
+     //get an array for just the keywords and get an array for all the horn options (Diyana helped here)
+     const keywordsArray = images.map(entry=>entry.keyword)
+     const hornsArray = images.map(entry=>entry.horns) 
+     return (
+      <div className ="main">        
         <Header />
-        {/*need a dropdown to create filter option, establish state on line 7.5, */} 
-        <select onChange= {this.handleChange}>
-          <option value ="">All</option>
-          <option value ="uniWhal">UniWhal</option>
-          <option value ="rhino">Rhino</option>
-          <option value ="unicorn">Unicorn</option>
-          <option value ="uniLego"> Unilego</option>
-          <option value ="narwhal"> Narwhal</option>
-          <option value ="triceratops"> Triceratops</option>
-          <option value ="markor">Markor</option>
-          <option value ="mouflon">Mouflan</option>
-          <option value ="chameleon">Chameleon</option>
-          <option value ="lizard">Lizard</option>
-          <option value ="dragon">Dragon</option>
-          </select>
-        <ul>
-        {
-          filteredImages.map(image =>
-            <ImageList 
-              url = {image.url}
-              title = {image.title}
-              description = {image.description}
-              keyword = {image.keyword}
-              horns ={image.horns} 
-            />)
-        }
-        </ul>
-      </>
-    );
+                 <Dropdown option={keywordsArray} select={this.state.keyword} handleChange = {this.handleKeyword} />
+                  
+               
+                  <Dropdown option ={hornsArray} select={this.state.horns} handleChange = {this.handleHorns} /> 
+                <ImageList filteredImages={filteredImages}/>
+                 
+        </div>
+    )
   }
 }
